34001c91ad4269f1f9c583ada1492b8093a57a2a
Anthony Ricketts <mra.ricketts@gmail.com>
2024-10-16 09:26:55 +0100

    only thing the is not work is taking from the inputs and showing the database

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+
+export default function About() {
+  const [formValues, setFormValues] = useState({
+    username: "",
+    description: "",
+    list: "",
+  });
+  const [selectedFruit, setSelectedFruit] = useState("low");
+  function handleFormValuesChange(event) {
+    setFormValues({ ...formValues, [event.target.name]: event.target.value });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  // fetch("url", {
+  //       method: "POST",
+  //   body: formValues,
+  //   headers: {
+  //       "Content-Type": "application/json"
+  //   }
+  //   })
+  //     }
+  return (
+    <div>
+      <h1>Form</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          placeholder="Name"
+          required
+          value={formValues.username}
+          onChange={handleFormValuesChange}
+        />
+        <label htmlFor="description">Description</label>
+        <input
+          type="text"
+          id="description"
+          name="description"
+          placeholder="description"
+          required
+          value={formValues.description}
+          onChange={handleFormValuesChange}
+        />
+        <label
+          htmlFor="list"
+          name="list"
+          onChange={handleFormValuesChange}
+          value={formValues.list}
+          required
+        >
+          Category:
+          <select
+            value={selectedFruit}
+            onChange={(e) => setSelectedFruit(e.target.value)}
+          >
+            <option value="low">Low</option>
+            <option value="moderate">Moderate</option>
+            <option value="substantial">Substantial</option>
+            <option value="critical">Critical</option>
+          </select>
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      <p>Current user: {formValues.username}</p>
+      <p>User description: {formValues.description}</p>
+      <p>Category: {formValues.list}</p>
+      <p>Level: {selectedFruit}</p>
+    </div>
+  );
+}
